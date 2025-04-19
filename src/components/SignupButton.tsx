b20c7902adb335e21c4fb347819c69de99ec28cf
armanderete <armanderete@gmail.com>
2025-04-19 16:52:53 -0600

    min

--- src/components/SignupButton.tsx
+++ src/components/SignupButton.tsx
@@ -6,6 +6,8 @@ export default function SignupButton() {
     <WalletWrapper
       className="ockConnectWallet_Container min-w-[90px] shrink bg-slate-200 text-[#030712] hover:bg-slate-300"
       text="Sign up"
+      basenamesCache={{}}
+      isHolder={false}
     />
   );
 }
